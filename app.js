978bf28cc2380e941bea52cd9cdf75d6a0785114
Karan Babar <karan.babar2004@gmail.com>
2026-02-26 22:53:24 +0530

    Added span Typed effect

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const submitBtn = document.querySelector('.submit-btn');
 const formInputs = document.querySelectorAll('.form-input, .form-textarea');
 const form = document.getElementById('contactForm');
 
+var typed = new Typed(".text", {
+  strings: ["Full Stack Developer", "MERN Stack", "Software Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+
 submitBtn.addEventListener('click', (e) => {
 
     let isValid = true;
